a0d8f888d15b8b776e658aa960f1c58aff29e4d1
Giovanni Chahin Morassi <giovannimorassi13511@gmail.com>
2025-11-01 18:37:35 -0300

    .\

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 
 from core.database import create_db_and_tables, init_astra_cassandra
@@ -22,13 +23,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3001", "http://127.0.0.1:3001"],
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 app.include_router(token.router)
 app.include_router(user.router)
 app.include_router(payment_methods.router)
